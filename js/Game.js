09e67c981b15164fc4be633a21ab0ba597807eff
sjpriya <72385035+sjpriya@users.noreply.github.com>
2021-02-17 14:12:53 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -30,13 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
-    car1.addImage("car1",car1_img);
+   
     car2 = createSprite(300,200);
-    car2.addImage("car2",car2_img);
+    
     car3 = createSprite(500,200);
-    car3.addImage("car3",car3_img);
+    
     car4 = createSprite(700,200);
-    car4.addImage("car4",car4_img);
+    
     cars = [car1, car2, car3, car4];
   }
 
@@ -46,10 +46,9 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
-      background(rgb(198,135,103));
-      image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
+      // image for background
       
-      //var display_position = 100;
+    
       
       //index of the array
       var index = 0;
